f81dd09f222023d3d8313e8c11eca5de9544435c
baderbbm <131297523+baderbbm@users.noreply.github.com>
2024-02-02 14:56:20 +0100

    Add files via upload

---
+++ src/test/java/com/microservices/MedecinNoteControllerTest.java
@@ -0,0 +1,39 @@
+package com.microservices;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.microservices.controllers.MedecinNoteController;
+import com.microservices.domain.MedecinNote;
+import com.microservices.services.MedecinNoteService;
+
+@SpringBootTest
+public class MedecinNoteControllerTest {
+
+    @Test
+    public void testGetNotesByPatId() {
+        // Créez un objet Mock du service
+        MedecinNoteService medecinNoteService = mock(MedecinNoteService.class);
+
+        // Créez un objet Controller avec le service Mock
+        MedecinNoteController medecinNoteController = new MedecinNoteController(medecinNoteService);
+
+        // Définissez le comportement du service Mock lors de l'appel à getNotesByPatId
+        when(medecinNoteService.getNotesByPatId(1)).thenReturn(Arrays.asList(
+                new MedecinNote("1", 1, "TestNone", "Le patient déclare qu'il 'se sent très bien'. Poids égal ou inférieur au poids recommandé"),
+                new MedecinNote("2", 2, "TestBorderline", "Le patient déclare qu'il ressent beaucoup de stress au travail. Il se plaint également que son audition est anormale dernièrement")
+        ));
+
+        // Appelez la méthode du contrôleur pour le test
+        List<MedecinNote> notes = medecinNoteController.getNotesByPatId(1);
+
+        // Vérifiez le résultat attendu
+        assertEquals(2, notes.size());
+        assertEquals("Le patient déclare qu'il 'se sent très bien'. Poids égal ou inférieur au poids recommandé", notes.get(0).getNote());
+        assertEquals("Le patient déclare qu'il ressent beaucoup de stress au travail. Il se plaint également que son audition est anormale dernièrement", notes.get(1).getNote());
+    }
+}
