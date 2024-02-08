61c170480fd7f26ebfe54a1fe7a8f5cc2828553e
baderbbm <131297523+baderbbm@users.noreply.github.com>
2024-02-08 21:36:17 +0100

    Update MedecinNoteController.java

--- src/main/java/com/microservices/controllers/MedecinNoteController.java
+++ src/main/java/com/microservices/controllers/MedecinNoteController.java
@@ -1,13 +1,15 @@
 package com.microservices.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.microservices.domain.MedecinNote;
 import com.microservices.services.MedecinNoteService;
-
 import java.util.List;
 
 @RestController
@@ -23,5 +25,16 @@ public class MedecinNoteController {
 	@GetMapping("/medecin/notes/{patId}")
 	public List<MedecinNote> getNotesByPatId(@PathVariable int patId) {
 		return medecinNoteService.getNotesByPatId(patId);
+	}
+	
+	@PostMapping("/medecin/notes/{patId}")
+	public ResponseEntity<MedecinNote> addMedecinNote(@RequestBody MedecinNote medecinNote, @PathVariable int patId) {
+	    try {
+	        medecinNote.setPatId(patId);
+	        MedecinNote addedNote = medecinNoteService.addMedecinNote(medecinNote);
+	        return ResponseEntity.ok(addedNote);
+	    } catch (Exception e) {
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+	    }
 	}
 }
