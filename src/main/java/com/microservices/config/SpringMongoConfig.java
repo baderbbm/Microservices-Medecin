2566a565a06bfa8442a49503626f1ec7c6a4132a
baderbbm <131297523+baderbbm@users.noreply.github.com>
2024-02-09 17:58:23 +0100

    Update SpringMongoConfig.java

--- src/main/java/com/microservices/config/SpringMongoConfig.java
+++ src/main/java/com/microservices/config/SpringMongoConfig.java
@@ -1,1 +1,19 @@
+package com.microservices.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class SpringMongoConfig{
+	
+	@Autowired
+	private MappingMongoConverter mappingMongoConverter;
+
+	@PostConstruct
+	public void configureMappingMongoConverter() {
+	    this.mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+	}	
+}
