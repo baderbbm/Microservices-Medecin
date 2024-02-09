5b8278e2813b0ae6efc393eeeedcf01ebb6a4e31
baderbbm <131297523+baderbbm@users.noreply.github.com>
2024-02-09 18:11:10 +0100

    Update MedecinNoteControllerTest.java

--- src/test/java/com/microservices/MedecinNoteControllerTest.java
+++ src/test/java/com/microservices/MedecinNoteControllerTest.java
@@ -1,12 +1,16 @@
 package com.microservices;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import com.microservices.controllers.MedecinNoteController;
 import com.microservices.domain.MedecinNote;
 import com.microservices.services.MedecinNoteService;
@@ -35,5 +39,49 @@ public class MedecinNoteControllerTest {
         assertEquals(2, notes.size());
         assertEquals("Le patient déclare qu'il 'se sent très bien'. Poids égal ou inférieur au poids recommandé", notes.get(0).getNote());
         assertEquals("Le patient déclare qu'il ressent beaucoup de stress au travail. Il se plaint également que son audition est anormale dernièrement", notes.get(1).getNote());
+    }
+    
+    @Test
+    public void testAddMedecinNote() {
+        // Créez un objet Mock du service
+        MedecinNoteService medecinNoteService = mock(MedecinNoteService.class);
+
+        // Créez un objet Controller avec le service Mock
+        MedecinNoteController medecinNoteController = new MedecinNoteController(medecinNoteService);
+
+        // Créez un objet MedecinNote à ajouter
+        MedecinNote medecinNoteToAdd = new MedecinNote("1", 1, "TestAddNote", "This is a test note to add");
+
+        // Définissez le comportement du service Mock lors de l'appel à addMedecinNote
+        when(medecinNoteService.addMedecinNote(any(MedecinNote.class))).thenReturn(medecinNoteToAdd);
+
+        // Appelez la méthode du contrôleur pour le test
+        ResponseEntity<MedecinNote> response = medecinNoteController.addMedecinNote(medecinNoteToAdd, 1);
+
+        // Vérifiez le résultat attendu
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(medecinNoteToAdd, response.getBody());
+    }
+    
+    @Test
+    public void testAddMedecinNoteWithException() {
+        // Créez un objet Mock du service
+        MedecinNoteService medecinNoteService = mock(MedecinNoteService.class);
+
+        // Créez un objet Controller avec le service Mock
+        MedecinNoteController medecinNoteController = new MedecinNoteController(medecinNoteService);
+
+        // Créez un objet MedecinNote à ajouter
+        MedecinNote medecinNoteToAdd = new MedecinNote("1", 1, "TestAddNote", "This is a test note to add");
+
+        // Définissez le comportement du service Mock lors de l'appel à addMedecinNote pour lancer une exception
+        when(medecinNoteService.addMedecinNote(medecinNoteToAdd)).thenThrow(new RuntimeException());
+
+        // Appelez la méthode du contrôleur pour le test
+        ResponseEntity<MedecinNote> response = medecinNoteController.addMedecinNote(medecinNoteToAdd, 1);
+
+        // Vérifiez le résultat attendu
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertEquals(null, response.getBody()); // Le corps de la réponse doit être null 
     }
 }
